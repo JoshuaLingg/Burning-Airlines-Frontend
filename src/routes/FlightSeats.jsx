e267fddb80f964349a1321dc3ae8c9ba9809b4b2
JoshuaLing <joshualing81@gmail.com>
2023-05-10 12:53:24 +1000

    added seatselection component to flightseats

--- src/routes/FlightSeats.jsx
+++ src/routes/FlightSeats.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
+import SeatSelection from "../components/SeatSelection";
 
 function FlightSeats() {
   return (
     <div className="container">
       <Navbar />
 
+      <SeatSelection />
       <p>Seat selection is coming soon</p>
     </div>
   );
