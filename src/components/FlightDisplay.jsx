16a1da3c6542e7c752b26062983e63876f7e478a
Howard Wang <haoyu.au@gmail.com>
2023-05-11 13:16:53 +1000

    some changes

--- src/components/FlightDisplay.jsx
+++ src/components/FlightDisplay.jsx
@@ -1,35 +1,59 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const Server_Url = 'http://localhost:3000/reservations.json'
+
 function FlightDisplay() {
-  const flights = [
-    {
-      Date: '3/1/13',
-      Flight: 23,
-      FromTo: 'JFK > SFO',
-      Plane: 757,
-      Seat: 32
-    }
-  ]
+  const [flights, setFlights] = useState([]);
+
+  const fetchFlights = () => {
+    axios(Server_Url).then((response) => {
+     setFlights(response.data) 
+  })
+  }
+  useEffect(fetchFlights, [])
+  
+  
   return (
     <div>
-      <h1>Flights</h1>
+     <FlightsList flights={flights} />
+    </div>
+  )
+}
+const FlightsList = (props) => {
+  return (
+      <div>
+         <h1>Flights</h1>
       <table>
-        <tr>
+        
+        <thead>
+          <tbody>
           <th>Date</th>
           <th>Flight</th>
           <th>From {">"} To</th>
           <th>Plane</th>
           <th>Seat</th>
-        </tr>
-        <tr>
-        <td>{flights[0].Date}</td>
-        <td>{flights[0].Flight}</td>
-        <td>{flights[0].FromTo}</td>
-        <td>{flights[0].Plane}</td>
-        <td>{flights[0].Seat}</td>
-        </tr>
+          </tbody>
+        </thead>
+       {props.flights.map((f) => {
+        return (
+        <tbody key={f.id}>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>{f.seat}</td>
+        </tbody>
+        )
+       })}
+   
+        
+        
  
       </table>
-    </div>
+      </div>
   );
 };
+
 
 export default FlightDisplay;
